4e5dd591ed2492cd3f040d0164a2be424161f7b0
admin <admin>
2020-06-04 14:49:18 -0400

    Deleted dataset LOAN_DEFAULT.sql_variable_creation

--- recipes/compute_sql_variable_creation.sql
+++
@@ -1,6 +0,0 @@
---defines variable "max_income"
---WITH max_income AS (select max("Monthly_Income") from "LOAN_DEFAULT_customer_information_postgres")
-
---use variable in select statement
-SELECT "id", "Status", 12 AS total FROM "LOAN_DEFAULT_customer_information_postgres"
-
