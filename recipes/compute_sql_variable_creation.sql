95d202c19b3158ad2abb412f63ef6db6daabca0c
admin <admin>
2020-06-04 12:10:39 -0400

    Saved recipe: LOAN_DEFAULT.compute_sql_variable_creation

--- recipes/compute_sql_variable_creation.sql
+++ recipes/compute_sql_variable_creation.sql
@@ -1,7 +1,6 @@
+--defines variable "max_income"
+WITH max_income AS (select max("Monthly_Income") from "LOAN_DEFAULT_customer_information_postgres")
 
+--use variable in select statement
+SELECT 'id', 'Status', 12 AS total FROM "LOAN_DEFAULT_customer_information_postgres"
 
-
---SELECT *
---  FROM "LOAN_DEFAULT_customer_information_postgres"
-
-select max("Monthly_Income") as max_income from "LOAN_DEFAULT_customer_information_postgres"
