6d6764ee724725898c397e89483ed76407cc4377
admin <admin>
2020-06-04 11:46:03 -0400

    Saved recipe: LOAN_DEFAULT.compute_sql_variable_creation

--- recipes/compute_sql_variable_creation.sql
+++ recipes/compute_sql_variable_creation.sql
@@ -4,4 +4,4 @@
 --SELECT *
 --  FROM "LOAN_DEFAULT_customer_information_postgres"
 
-select max(Monthly_Income) as max_income from "LOAN_DEFAULT_customer_information_postgres"
+select max("Monthly_Income") as max_income from "LOAN_DEFAULT_customer_information_postgres"
