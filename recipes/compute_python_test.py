dfd71adea490fdccf9e5491a376a28836d28ac34
admin <admin>
2020-08-06 17:07:27 -0400

    Recipe LOAN_DEFAULT.compute_python_test saved through notebook notebook editor for compute_python_test

--- recipes/compute_python_test.py
+++ recipes/compute_python_test.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,14 +6,24 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 all_loan_requests_final_prepared = dataiku.Dataset("all_loan_requests_final_prepared")
-all_loan_requests_final_prepared_df = all_loan_requests_final_prepared.get_dataframe()
+df = all_loan_requests_final_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from custom_functions import get_today
+the_date = get_today()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+the_date
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['date'] = the_date
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-python_test_df = all_loan_requests_final_prepared_df # For this sample code, simply copy input to output
+python_test_df = df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
