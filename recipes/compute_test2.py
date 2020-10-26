97fda6374dbb5f78f0b07a4d9a9037cdea2b40a5
admin <admin>
2020-10-26 09:20:35 -0400

    Recipe LOAN_DEFAULT.compute_test2 saved through notebook notebook editor for compute_test2_1

--- recipes/compute_test2.py
+++ recipes/compute_test2.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,16 +6,22 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 all_loan_requests_final_prepared = dataiku.Dataset("all_loan_requests_final_prepared")
-all_loan_requests_final_prepared_df = all_loan_requests_final_prepared.get_dataframe()
+df = all_loan_requests_final_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['index'] = df.index
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-test2_df = all_loan_requests_final_prepared_df # For this sample code, simply copy input to output
+#test2_df = all_loan_requests_final_prepared_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
 test2 = dataiku.Dataset("test2")
-test2.write_with_schema(test2_df)
+test2.write_with_schema(df)
