f2f4cb33cc2f92d49a7831b7ec82413f39168885
admin <admin>
2023-02-06 10:17:05 -0500

    Recipe GIT_DEMO.compute_python_out saved through notebook notebook editor for compute_python_out

--- recipes/compute_python_out.py
+++ recipes/compute_python_out.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,16 +6,22 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 all_loan_requests_final_prepared_filtered = dataiku.Dataset("all_loan_requests_final_prepared_filtered")
-all_loan_requests_final_prepared_filtered_df = all_loan_requests_final_prepared_filtered.get_dataframe()
+df = all_loan_requests_final_prepared_filtered.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['new'] = 1
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-python_out_df = all_loan_requests_final_prepared_filtered_df # For this sample code, simply copy input to output
+#python_out_df = all_loan_requests_final_prepared_filtered_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
 python_out = dataiku.Dataset("python_out")
-python_out.write_with_schema(python_out_df)
+python_out.write_with_schema(df)
